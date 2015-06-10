5bdde7da7d254436c746ffc014f82ac29472635f
AEspinet <A.Espinet@gmail.com>
2015-06-09 20:44:18 -0700

    Basic animations + cleaning markup

--- adamespinet2015/js/adamespinet.js
+++ adamespinet2015/js/adamespinet.js
@@ -1,35 +1,43 @@
 $(document).ready(function(){
 
-  $('#main-wrap').fadeIn(50);
 
-  $(".icon, #content, .about, #about-content").addClass("hide");
+  $("#wrap-top, #portfolio, #wrap-bottom, .icon, #content, .about, #about-content, #write-up, #photo-container").addClass("hide");
 
-  $("#copy-portfolio").removeClass("hide");
-  $("#copy-portfolio").addClass("animated fadeInDown delay show");
+  $("#write-up").removeClass("hide");
+  $("#write-up").addClass("pageAnimation fadeInDown show");
+  
+  $("#photo-container").removeClass("hide");
+  $("#photo-container").addClass("pageAnimation fadeInLeft show");
 
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+  $("#wrap-top").removeClass("hide");
+  $("#wrap-top").addClass("pageAnimation fadeInDown show");
 
-    if (scroll >= 600) {
-        $(".icon").removeClass("hide");
-        $(".icon").addClass("animated fadeInDown show");
+  $("#portfolio").removeClass("hide");
+  $("#portfolio").addClass("animated fadeIn show");
 
-        $("#content").removeClass("hide");
-        $("#content").addClass("animated fadeIn delay show");
-    }
-    if (scroll >= 2500) {
-        $(".about").removeClass("hide");
-        $(".about").addClass("animated fadeInDown show");
-
-        $("#about-content").removeClass("hide");
-        $("#about-content").addClass("animated fadeInDown show");
-
-    }
-});
+  $("#wrap-bottom").removeClass("hide");
+  $("#wrap-bottom").addClass("pageAnimation fadeInUp show");
 
 
+  $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
 
+      if (scroll >= 600) {
+          $(".icon").removeClass("hide");
+          $(".icon").addClass("animated fadeInDown show");
 
+          $("#content").removeClass("hide");
+          $("#content").addClass("animated fadeIn delay show");
+      }
+      if (scroll >= 2500) {
+          $(".about").removeClass("hide");
+          $(".about").addClass("animated fadeInDown show");
+
+          $("#about-content").removeClass("hide");
+          $("#about-content").addClass("animated fadeInDown show");
+
+      }
+  });
 
 
 });
